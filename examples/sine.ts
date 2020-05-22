2ee1b29877a0690992d5402f7fe35ad5dd0dd50e
Divy Srivastava <dj.srivastava23@gmail,com>
2020-05-22 18:49:35 +0530

    feat: move mouse using `.moveMouse`

---
+++ examples/sine.ts
@@ -0,0 +1,14 @@
+import AutoPilot from 'https://raw.githubusercontent.com/divy-work/autopilot-deno/master/mod.ts';
+
+var pilot = new AutoPilot();
+var twoPI = Math.PI * 2.0;
+var screenSize = pilot.screenSize();
+var height = (screenSize.height / 2) - 10;
+var width = screenSize.width;
+
+var y;
+for (var x = 0; x < width; x++)
+{
+	y = height * Math.sin((twoPI * x) / width) + height;
+	pilot.moveMouse(x, y);
+}
