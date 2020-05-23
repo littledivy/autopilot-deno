4048e5e39c08abb824de0b72ad07a3c96ffe2b65
Divy Srivastava <dj.srivastava23@gmail.com>
2020-05-23 18:13:10 +0530

    fix: doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -5,21 +5,21 @@
 
 extern crate rs_lib;
 
-//! use deno_core and futures
+// use deno_core and futures
 use deno_core::plugin_api::Buf;
 use deno_core::plugin_api::Interface;
 use deno_core::plugin_api::Op;
 use deno_core::plugin_api::ZeroCopyBuf;
 use futures::future::FutureExt;
 
-//! use serde
+// use serde
 use serde::Deserialize;
 use serde::Serialize;
 
 use rs_lib::geometry::{Point, Rect, Size};
 use std::path::Path;
 
-//! register all ops here
+// register all ops here
 #[no_mangle]
 pub fn deno_plugin_init(interface: &mut dyn Interface) {
   interface.register_op("type", op_type);
@@ -29,16 +29,16 @@ pub fn deno_plugin_init(interface: &mut dyn Interface) {
   interface.register_op("screenshot", op_screen_shot);
 }
 
-//! deno bindings for `type`
+// deno bindings for `type`
 fn op_type(
   _interface: &mut dyn Interface,
   data: &[u8],
   zero_copy: Option<ZeroCopyBuf>,
 ) -> Op {
-  /// convert arg to string
+  // convert arg to string
   let data_str = std::str::from_utf8(&data[..]).unwrap().to_string();
 
-  /// in case, we need a optional functionality in future
+  // in case, we need a optional functionality in future
   let fut = async move {
     if let Some(buf) = zero_copy {
       let buf_str = std::str::from_utf8(&buf[..]).unwrap();
@@ -49,14 +49,14 @@ fn op_type(
     }
     let (tx, rx) = futures::channel::oneshot::channel::<Result<(), ()>>();
     std::thread::spawn(move || {
-      /// call type_string
+      // call type_string
       rs_lib::key::type_string(&data_str, &[], 200., 0.);
       std::thread::sleep(std::time::Duration::from_secs(1));
       tx.send(Ok(())).unwrap();
   });
     assert!(rx.await.is_ok());
 
-    /// return true
+    // return true
     let result = b"true";
     let result_box: Buf = Box::new(*result);
     result_box
@@ -65,14 +65,14 @@ fn op_type(
   Op::Async(fut.boxed())
 }
 
-/// struct for options used by Alert
+// struct for options used by Alert
 #[derive(Deserialize)]
 struct AlertOptions {
     msg: str,
     title: str
 }
 
-//! deno bindings for `alert`
+// deno bindings for `alert`
 fn op_alert(
   _interface: &mut dyn Interface,
   data: &[u8],
