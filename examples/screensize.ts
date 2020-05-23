3fc46a4b5061d53521201b9fa6284aa1c6140e5b
Divy Srivastava <dj.srivastava23@gmail.com>
2020-05-23 17:27:15 +0530

    feat: add example for screensize

---
+++ examples/screensize.ts
@@ -0,0 +1,9 @@
+import AutoPilot from 'https://raw.githubusercontent.com/divy-work/autopilot-deno/master/mod.ts';
+
+var pilot = new AutoPilot();
+var screenSize = pilot.screenSize();
+
+var height = screenSize.height;
+var width = screenSize.width;
+
+console.log(`width: ${width}\nheight: ${height}`)
