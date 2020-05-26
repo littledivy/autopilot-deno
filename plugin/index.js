9291319a8799520eeae69aa7ac95f8ba181c8995
Divy Srivastava <dj.srivastava23@gmail.com>
2020-05-26 14:45:33 +0530

    fix: release link

--- plugin/index.js
+++ plugin/index.js
@@ -3,7 +3,7 @@ import { prepare } from 'https://raw.githubusercontent.com/manyuanrong/deno-plug
 const filenameBase = "autopilot_deno";
 
 const PLUGIN_URL_BASE =
-  "https://github.com/divy-work/autopilot-deno/latest/download";
+  "https://github.com/divy-work/autopilot-deno/releases/latest/download";
 
 const isDev = Deno.env.get("DEV");
 
