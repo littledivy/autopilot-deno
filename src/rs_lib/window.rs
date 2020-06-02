6f46a4a7507cac627c52c0d9e9c159b2d3a48f99
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-02 13:10:48 +0530

    feat: try `.getMonitors` for macOS

--- src/rs_lib/window.rs
+++ src/rs_lib/window.rs
@@ -42,6 +42,7 @@ pub fn get_window(index: usize) -> String {
     String::from("null")
 }
 
+// xrandr --listactivemonitors
 #[cfg(target_os = "linux")]
 pub fn get_active_monitors() -> String {
     let output = Command::new("xrandr")
@@ -57,6 +58,7 @@ pub fn get_active_monitors() -> String {
     }
 }
 
+// cmd /C monitors.bat
 #[cfg(target_os = "windows")]
 pub fn get_active_monitors() -> String {
     let output = Command::new("cmd")
@@ -72,8 +74,18 @@ pub fn get_active_monitors() -> String {
     }
 }
 
+// system_profiler SPDisplaysDataType
 #[cfg(target_os = "macos")]
 pub fn get_active_monitors() -> String {
-    println!("{}", "get_monitors is not supported for MacOS");
-    String::from("Monitors: 1")
+    let output = Command::new("system_profiler")
+                     .arg("SPDisplaysDataType")
+                     .output()
+                     .expect("failed to execute process");
+    let active_monitors_cli = String::from_utf8_lossy(&output.stdout);
+    println!("{}", String::from_utf8_lossy(&output.stderr));
+    if !active_monitors_cli.is_empty() {
+        active_monitors_cli.to_string()
+    } else {
+        String::from("Monitors: 1")
+    }
 }
