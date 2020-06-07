56cf2318921e8d6028166d7639cdd60ccf59fb9c
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-07 14:56:42 +0530

    feat: add Deno alt

---
+++ lib/monitors.ts
@@ -0,0 +1,44 @@
+// monitors.ts (this file was an idea that has been implemented in windows.rs)
+
+const WinArgs = [
+  "cmd",
+  "/C",
+  "monitors.bat"
+];
+
+const MacArgs = [
+  "system_profiler",
+  "SPDisplaysDataType"
+]
+
+const LinuxArgs = [
+  "xrandr",
+  "--listactivemonitors"
+]
+
+async function runCommand(cliArgs: string[]): Promise<string> {
+  const process = Deno.run({
+    cmd: [...cliArgs],
+    cwd: "utils",
+    stdout: "piped",
+  });
+  const { code } = await process.status();
+  if (code === 0) {
+    const rawOutput = await process.output();
+    process.close();
+    return new TextDecoder().decode(rawOutput);
+  } else {
+    process.close();
+    throw new Error(`Process exited with error code ${code}`);
+  }
+}
+
+export async function getMonitors() {
+  switch (Deno.build.os) {
+    case "linux":
+      const res = await runCommand(LinuxArgs);
+      return res.split("\n")[0].split("Monitors:").join("").trim()
+      break;
+
+  }
+}
