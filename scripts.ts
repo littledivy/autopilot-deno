b643b6ffaf06fe58910563822c9f4b737702bb76
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-09 20:14:54 +0530

    chores: use vr ts scripts

---
+++ scripts.ts
@@ -0,0 +1,11 @@
+// Copyright 2020-present Divy Srivastava and friends. All rights reserved. MIT license.
+// Scripts for velociraptor
+
+export default {
+  "scripts": {
+    "build": "cargo build",
+    "benchmarks": "DEV=* DEBUG=* deno run --unstable -A benchmarks/index.ts",
+    "fmt": "deno fmt && cargo fmt",
+    "test": "deno test --unstable -A"
+  }
+}
