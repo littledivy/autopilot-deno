9a4a4fbd87119a265e4c621c5d4898acf55a0e34
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-10 15:07:30 +0530

    chores: sort deps

--- deps.ts
+++ deps.ts
@@ -8,17 +8,21 @@ import {
   jsonFormat,
   textFormat,
 } from "https://deno.land/x/deno_structured_logging@0.4.1/mod.ts";
-export {
-  runBenchmarks,
-  bench,
-} from "https://deno.land/std@0.56.0/testing/bench.ts";
-export {
-  prettyBenchmarkResult,
-  prettyBenchmarkProgress,
-} from "https://deno.land/x/pretty_benching/mod.ts";
+
 export { prepare } from "https://raw.githubusercontent.com/manyuanrong/deno-plugin-prepare/master/mod.ts";
 
 export const logger = createLogger({
   minimumLevel: Deno.env.get("DEBUG") ? LogLevel.DEBUG : LogLevel.INFO,
   outputFormat: textFormat,
 }).addSink(consoleSink());
+
+// Benchmark exports
+export {
+  runBenchmarks,
+  bench,
+} from "https://deno.land/std@0.56.0/testing/bench.ts";
+
+export {
+  prettyBenchmarkResult,
+  prettyBenchmarkProgress,
+} from "https://deno.land/x/pretty_benching/mod.ts";
