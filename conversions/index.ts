f7da6e3e02fb0fc1d851dbb3ef930533b9bb75e8
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-24 14:42:12 +0530

    chores: fmt

--- conversions/index.ts
+++ conversions/index.ts
@@ -1,5 +1,8 @@
 // type conversion for Autopilot's methods
-export default function safeConvert(from: string, to: "number" | "object"): null | number | object {
+export default function safeConvert(
+  from: string,
+  to: "number" | "object",
+): null | number | object {
   switch (to) {
     case "number":
       return Number(from);
--- conversions/methods.ts
+++ conversions/methods.ts
@@ -1,8 +1,8 @@
 export default {
   "number": [
-      "moveMouse",
-      "pointVisible",
-      "getWindow",
-      "transformByIndex"
-  ]
-}
+    "moveMouse",
+    "pointVisible",
+    "getWindow",
+    "transformByIndex",
+  ],
+};
